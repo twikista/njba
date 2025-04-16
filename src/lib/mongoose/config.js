2288476ccd8a86fe2b81bd7e9cfe0266121dd1d6
Aaron Anama <twikista@gmail.com>
2025-04-16 21:29:22 +0100

    added views and download properties to article model

--- src/lib/mongoose/config.js
+++ src/lib/mongoose/config.js
@@ -4,13 +4,13 @@ const connection = {};
 export const connectDB = async () => {
   try {
     if (connection.isConnected) {
-      // console.log('using exisiting connection')
+      console.log('using exisiting connection');
     }
 
     const db = await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI);
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
-    // console.log('failed to connect')
+    console.log('failed to connect');
     // console.log(error)
   }
 };
