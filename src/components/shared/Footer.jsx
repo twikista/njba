32aa86defad56d8bb1ee631c776889252ee3c658
Aaron Anama <twikista@gmail.com>
2025-08-31 11:23:12 +0100

    added whatsapp link

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -5,10 +5,16 @@ import { FaCreativeCommonsBy, FaCreativeCommons } from 'react-icons/fa';
 import { useSelectedLayoutSegment } from 'next/navigation';
 import { footerItems } from '@/static/menuItems';
 import uniqid from 'uniqid';
+import { HiMiniArrowTopRightOnSquare } from 'react-icons/hi2';
+import { RxOpenInNewWindow } from 'react-icons/rx';
 
 function Footer() {
   const routeSegment = useSelectedLayoutSegment();
   const currentYear = new Date().getFullYear();
+  const preFillMessage = encodeURIComponent(
+    'Hello, I would like to make some enquiries about your services.'
+  );
+  const whatsappUrl = `https://wa.me/2348138007003?text=${preFillMessage}`;
   return (
     <>
       {routeSegment !== 'dashboard' && (
@@ -44,12 +50,24 @@ function Footer() {
             <span className='h-[1px] bg-[#fafafa]/50 block' />
             <div className='flex flex-col items-center'>
               <div className='text-xs text-center ms:text-sm sm:text-base'>
-                <div className='flex items-center text-'>
-                  <span className='uppercase'>&copy;MSR</span>
+                <div className=' items-center space-y-3 text-'>
+                  <span className=''>
+                    &copy; {currentYear} Management Sciences Review.
+                  </span>
                   <span>&nbsp;</span>
-                  <span>{currentYear}</span>
-                  <span>&nbsp;&#124;&nbsp;</span>
-                  <span className=''>Powered by Inventure Solutions</span>
+                  {/* <span>&nbsp;&#124;&nbsp;</span> */}
+                  {/* <a href='https://wa.me/2347038823825' target='_blank' rel='noopener noreferrer'>
+                    reach me
+                  </a> */}
+                  <a
+                    className='flex items-center justify-center gap-[2px] hover:underline underline-offset-2'
+                    href={whatsappUrl}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Powered by Inventure Solutions
+                    <RxOpenInNewWindow />
+                  </a>
                 </div>
               </div>
             </div>
